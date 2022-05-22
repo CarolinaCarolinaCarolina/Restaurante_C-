004467b10893aec03a84c9154bb89766fe30a30e
Santiago Suarez <Santiago32296@gmail.com>
2022-05-22 16:40:35 -0500

    error ya

---
+++ Restaurante_Csharp/CONEXION/ConexionMaestra.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sistema_para_restaurante_en_CSHARP_codigo369.CONEXION
+{
+    class CONEXIONMAESTRA
+    {
+        public static string conexion = Convert.ToString(Restaurante_Csharp.LIBRERIAS.Desencriptar.checkServer());
+    }
+}
